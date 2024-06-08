c0e7b2be7ae86f338eb57c9e34ec95de769356da
Karol <karol.gladosz@gmail.com>
2024-06-09 00:14:19 +0200

    Dodanie kategorii produktów

---
+++ Script2.sql
@@ -0,0 +1,3 @@
+﻿-- Dodanie kategorii produktów do sklepu. Rozmiary i modele są uzależnione od kategorii produktu.
+INSERT INTO [dbo].[KategoriaProduktu] ([NazwaKategorii])
+VALUES ('Obuwie damskie'),('Obuwie meskie'),('Obuwie dzieciece'), ('Klapki'), ('Czapki meskie');
