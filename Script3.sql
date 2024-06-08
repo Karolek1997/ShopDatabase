bb7c96ed542b4a638859acd3febd5ec383993892
Karol <karol.gladosz@gmail.com>
2024-06-09 00:15:37 +0200

    Tabela walidacyjna rozmiary

---
+++ Script3.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE WalidacjaRozmiarow (
+    NazwaKategorii VARCHAR(50),
+    DostepneRozmiary VARCHAR(100));
+
+INSERT INTO WalidacjaRozmiarow (NazwaKategorii, DostepneRozmiary)
+VALUES 
+	('Obuwie damskie', '30, 31, 32, 33, 34, 35, 36, 37, 38, 39'),
+	('Obuwie meskie', '40, 41, 42, 43, 44, 45, 46, 47, 48'),
+	('Obuwie dzieciece', '20, 21, 22, 23, 24, 25, 26, 27, 28, 29'),
+	('Klapki', '25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45'),
+	('Czapki meskie', 'S, M, L, XL, Oversize');
