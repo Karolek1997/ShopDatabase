7f0470b992dd075ad3fc04252a4d0888aa081ae7
Karol <karol.gladosz@gmail.com>
2024-06-09 12:04:11 +0200

    Ukończony kod dla bazy sklepu

--- Script1.sql
+++ Script1.sql
@@ -9,16 +9,17 @@ CREATE TABLE [dbo].[Produkt](
 	[DostepnoscID] [int] NOT NULL);
  
  -- Stworzenie tabeli podrzędnej KategoriaProduktu
-CREATE TABLE [dbo].[KategoriaProduktu](
+CREATE TABLE [dbo].[KategoriaProduktu] (
 	[KategoriaProduktuID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[NazwaKategorii] [nvarchar](50) NOT NULL);
- 
+	[NazwaKategorii] [nvarchar](50) NOT NULL,
+	CONSTRAINT UQ_NazwaKategorii UNIQUE (NazwaKategorii));
+
 -- Stworzenie tabeli podrzędnej Dostepnosc
 CREATE TABLE [dbo].[Dostepnosc](
 	[DostepnoscID] [int] IDENTITY(1,1)PRIMARY KEY NOT NULL,
 	[DostepnaIlosc] [int] NULL);
  
--- Stworzenie tabeli Kolor + dodanie unikalności dla NazwyKoloru
+-- Stworzenie tabeli Kolor +  dodanie unikalności dla NazwyKoloru
 CREATE TABLE [dbo].[Kolor](
 	[KolorID] [int] IDENTITY(1,1)PRIMARY KEY NOT NULL,
 	[NazwaKoloru] [nvarchar](50) NOT NULL,
@@ -117,3 +118,88 @@ CREATE TABLE [dbo].[Klient](
 	[NumerTelefonu] [char](9) NOT NULL,
 CONSTRAINT [UC_Login] UNIQUE([Login]),
 CONSTRAINT [UC_Email] UNIQUE ([Email]));
+  
+-- Stworzenie tabeli Adres + połączenie z tabelą Klient na zasadzie jeden klient wiele adresów
+CREATE TABLE [dbo].[Adres](
+	[AdresID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[MiastoID] [int] NOT NULL,
+	[KodPocztowyID] [int] NOT NULL,
+	[NumerBudynku] [int] NOT NULL,
+	[NumerLokalu] [int] NOT NULL,
+	[KlientID] [int] NOT NULL,
+FOREIGN KEY (KlientID) REFERENCES Klient(KlientID));
+ 
+-- Stworzenie tabeli Zamowienia + połączenie z tabelą Klient na zasadzie jeden klient wiele zamówień oraz z tabelą Adres na zasadzie jeden adres wiele zamówień
+CREATE TABLE [dbo].[Zamowienia](
+	[ZamowienieID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[KlientID] [int] NOT NULL,
+	[DataZamowienia] [date] NOT NULL,
+	[WartoscZamowienia] [int] NOT NULL,
+	[AdresID] [int] NOT NULL,
+	[StatusZamowienia] [nvarchar] (50) NOT NULL,
+FOREIGN KEY (KlientID) REFERENCES Klient(KlientID),
+FOREIGN KEY (AdresID) REFERENCES Adres(AdresID));
+
+-- Stworzenie tabeli ZamowioneProdukty + połączenie z tabelami Zamowienia i Produkt na zasadzie klucza obcego złożonego przy uwzględnieniem nowego ZamowioneProduktyID dla kolejnych kombinacji
+CREATE TABLE [dbo].[ZamowioneProdukty](
+	[ZamowioneProduktyID] [int] IDENTITY(1,1) NOT NULL,
+	[ZamowienieID] [int] NOT NULL,
+	[ProduktID] [int] NOT NULL,
+	[IloscZamowionychProduktow] [int] NOT NULL,
+	[WartoscZamowionychProduktow] [int] NOT NULL,
+PRIMARY KEY (ZamowienieID, ProduktID),
+FOREIGN KEY (ZamowienieID) REFERENCES Zamowienia(ZamowienieID),
+FOREIGN KEY (ProduktID) REFERENCES Produkt(ProduktID));
+ 
+-- Stworzenie tabeli KodPocztowy, nadanie unikalnosci dla kodu pocztowego
+CREATE TABLE [dbo].[KodPocztowy](
+	[KodPocztowyID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[KodPocztowy] [char](6)  NOT NULL,
+	[MiastoID] [int],
+CONSTRAINT [UC_KodPocztowy] UNIQUE ([KodPocztowy]));   
+  
+-- Stworzenie tabeli Miasto
+CREATE TABLE [dbo].[Miasto](
+	[MiastoID] [int] IDENTITY(1,1)PRIMARY KEY NOT NULL,
+	[NazwaMiasta] [nvarchar] (50) NOT NULL,
+	[KrajID] [int] NOT NULL,
+	[UlicaID] [int] NOT NULL);      	
+ 
+-- Stworzenie tabeli Kraj, nadanie unikalnosci dla nazwy kraju
+CREATE TABLE [dbo].[Kraj](
+	[KrajID] [int] IDENTITY(1,1)PRIMARY KEY NOT NULL,
+	[NazwaKraju] [nvarchar] (50) NOT NULL,
+CONSTRAINT [UC_NazwaKraju] UNIQUE ([NazwaKraju]));
+ 
+-- Stworzenie tabeli Ulica, nadanie unikalnosci dla nazwy ulicy
+CREATE TABLE [dbo].[Ulica](
+	[UlicaID] [int] IDENTITY(1,1)PRIMARY KEY NOT NULL,
+	[NazwaUlicy] [nvarchar] (50) NOT NULL,
+CONSTRAINT [UC_NazwaUlicy] UNIQUE ([NazwaUlicy]));
+ 
+-- Polączenie tabeli KodPocztowy z tabela Miasto, tak aby kody pocztowe, były przypisane do konkretnych Miast
+ALTER TABLE [dbo].[KodPocztowy]
+ADD CONSTRAINT FK_Miasto_KodPocztowy
+FOREIGN KEY ([MiastoID]) REFERENCES [dbo].[Miasto]([MiastoID]);
+ 
+-- Polączenie tabeli Miasto z podrzędną tabelą Kraj
+ALTER TABLE [dbo].[Miasto]
+ADD CONSTRAINT FK_Miasto_Kraj
+FOREIGN KEY ([KrajID]) REFERENCES [dbo].[Kraj]([KrajID]);
+ 
+-- Polączenie tabeli Adres z podrzędną tabelą Miasto
+ALTER TABLE [dbo].[Adres]
+ADD CONSTRAINT FK_Adres_Miasto
+FOREIGN KEY ([MiastoID]) REFERENCES [dbo].[Miasto]([MiastoID]);
+ 
+-- Polączenie tabeli Adres z podrzędną tabelą KodPocztowy
+ ALTER TABLE [dbo].[Adres]
+ADD CONSTRAINT FK_Adres_Kod_Pocztowy
+FOREIGN KEY ([KodPocztowyID]) REFERENCES [dbo].[KodPocztowy]([KodPocztowyID]);
+ 
+-- Polączenie tabeli Miasto z podrzędną tabelą Ulica
+ALTER TABLE [dbo].[Miasto]
+ADD CONSTRAINT FK_Miasto_Ulica
+FOREIGN KEY ([UlicaID]) REFERENCES [dbo].[Ulica]([UlicaID]);
+
+-- END
