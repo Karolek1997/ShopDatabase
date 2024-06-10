0bde921ddc07fcb1a71d8a1f6e87abdfaf66d306
Karol <karol.gladosz@gmail.com>
2024-06-10 20:49:16 +0200

    Trigger na rozmiary produktów

---
+++ Script4.sql
@@ -0,0 +1,21 @@
+﻿-- Trigger sprawdzający dodawany rozmiar na podstawie tabeli walidacyjnej WalidacjaRozmiarow
+CREATE TRIGGER SprawdzRozmiar
+ON Rozmiar
+INSTEAD OF INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM inserted i
+        INNER JOIN KategoriaProduktu kp ON i.KategoriaProduktuID = kp.KategoriaProduktuID
+        LEFT JOIN WalidacjaRozmiarow wr ON kp.NazwaKategorii = wr.NazwaKategorii
+        WHERE CHARINDEX(i.Rozmiar, wr.DostepneRozmiary) = 0
+    )
+    BEGIN
+        RAISERROR('Nieprawidłowy rozmiar dla wskazanej kategorii produktu.', 16, 1);
+        RETURN; -- Zakończ działanie triggera bez wykonania wstawienia
+    END
+    INSERT INTO Rozmiar (KategoriaProduktuID, Rozmiar)
+    SELECT KategoriaProduktuID, Rozmiar
+    FROM inserted;
+END;
