4d69a30ad230ac3bb84712bd13c9cd3227d00443
Karol <karol.gladosz@gmail.com>
2024-06-11 22:30:11 +0200

    Trigger na obliczanie ceny brutto

---
+++ Script7.sql
@@ -0,0 +1,13 @@
+﻿-- Trigger obliczający cenę brutto na podstawie ceny netto i ustawionej stawki VAT dla wstawianych i aktualizowanych rekordów w tabeli Cena
+CREATE TRIGGER ObliczCeneBrutto
+ON Cena
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    UPDATE Cena
+    SET CenaBrutto = 
+		ISNULL(i.CenaNetto, 0)
+		+ ((ISNULL(i.CenaNetto, 0) * (ISNULL(i.StawkaVAT, 0) / 100.0)))
+    FROM inserted i
+    WHERE Cena.ProduktID = i.ProduktID;
+END;
