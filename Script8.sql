51b03a5f954ca504d417c1bb19e35d09c5319201
Karol <karol.gladosz@gmail.com>
2024-06-11 22:47:48 +0200

    dodawanie stawki VAT

---
+++ Script8.sql
@@ -0,0 +1,13 @@
+﻿-- uniwersalne dodanie wartosci VAT dla wszystkich istniejących produktów w tabeli Cena
+UPDATE Cena
+SET StawkaVAT = 12
+
+-- w przypadku np. zmiany stawki VAT dla danej kategorii produktu, należy wykonać UPDATE w danej KategoriiProduktu 
+-- np. dodanie 15% vatu dla wszystkich produktów w kategorii "Obuwie dziecice"
+
+UPDATE Cena
+SET Cena.StawkaVAT = 15
+FROM Cena
+JOIN Produkt ON Cena.ProduktID = Produkt.ProduktID
+JOIN KategoriaProduktu ON KategoriaProduktu.KategoriaProduktuID = Produkt.KategoriaProduktuID
+WHERE KategoriaProduktu.NazwaKategorii = 'Obuwie dzieciece';
