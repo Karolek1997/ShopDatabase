0718d13e3e6e9d7b58a7cdb561635e1307eab0cb
Karol <karol.gladosz@gmail.com>
2024-06-17 18:35:23 +0200

    Aktualizacja procedury

--- Procedura podsumowania sprzedaży.sql
+++ Procedura podsumowania sprzedaży.sql
@@ -2,12 +2,18 @@
 CREATE PROCEDURE PodsumujSprzedaz
 AS
 BEGIN
+    -- Zwracanie wyników podsumowania sprzedaży
     SELECT 
-        YEAR(DataZamowienia) AS RokSprzedazy,
-        SUM(WartoscZamowienia) AS DochodBrutto,
-        SUM(WartoscZamowienia * 0.88) AS DochodNetto,
-        COUNT(ZamowienieID) AS LiczbaZamowienZrealizowanych
+        YEAR(DataZamowienia) AS 'Rok Sprzedazy',
+        SUM(WartoscZamowienia) AS 'Dochod Brutto',
+        SUM(WartoscZamowienia * 0.88) AS 'Dochod Netto',
+        COUNT(ZamowienieID) AS 'Liczba Zamówień'
     FROM [dbo].[Zamowienia]
     WHERE StatusZamowienia = 'Zakonczone'
     GROUP BY YEAR(DataZamowienia);
 END;
+
+-- uruchomienie
+EXEC PodsumujSprzedaz;
+
+
