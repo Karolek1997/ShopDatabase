f8a3fd0f84611bcbd3e4014f70d18b7143f51594
Karol <karol.gladosz@gmail.com>
2024-06-17 17:54:26 +0200

    procedura podsumowania sprzedazy do buttona w VS

---
+++ Procedura podsumowania sprzedaży.sql
@@ -0,0 +1,13 @@
+﻿-- procedura podsumowująca sprzedaż w danym roky
+CREATE PROCEDURE PodsumujSprzedaz
+AS
+BEGIN
+    SELECT 
+        YEAR(DataZamowienia) AS RokSprzedazy,
+        SUM(WartoscZamowienia) AS DochodBrutto,
+        SUM(WartoscZamowienia * 0.88) AS DochodNetto,
+        COUNT(ZamowienieID) AS LiczbaZamowienZrealizowanych
+    FROM [dbo].[Zamowienia]
+    WHERE StatusZamowienia = 'Zakonczone'
+    GROUP BY YEAR(DataZamowienia);
+END;
