6ade327f65a937c1c3a57c7986c34ac7a9bc3ac6
Karol <karol.gladosz@gmail.com>
2024-06-20 21:35:55 +0200

    Procedura anulowania zamówienia

---
+++ Procedura anulowania zamówienia z poziomu sql.sql
@@ -0,0 +1,28 @@
+﻿-- Procedura anulowania zamowienia z poziomu sql, przywraca dostepnosc produktow, usuwa wpisy o zamowionych produktach
+CREATE PROCEDURE AnulowanieZamowienia
+    @KlientID INT,
+    @ZamowienieID INT
+AS
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Zamowienia WHERE ZamowienieID = @ZamowienieID AND KlientID = @KlientID)
+    BEGIN
+        RAISERROR ('Zamówienie nie istnieje lub nie należy do klienta', 16, 1);
+        RETURN;
+    END
+
+    UPDATE Dostepnosc
+    SET DostepnaIlosc = DostepnaIlosc + zp.IloscZamowionychProduktow
+    FROM Dostepnosc d
+    JOIN Produkt p ON d.DostepnoscID = p.DostepnoscID
+    JOIN ZamowioneProdukty zp ON p.ProduktID = zp.ProduktID
+    WHERE zp.ZamowienieID = @ZamowienieID;
+
+    DELETE FROM ZamowioneProdukty
+    WHERE ZamowienieID = @ZamowienieID;
+
+    UPDATE Zamowienia
+    SET StatusZamowienia = 'anulowane'
+    WHERE ZamowienieID = @ZamowienieID;
+
+    PRINT 'Zamówienie zostało anulowane, a dostępność produktów została przywrócona.';
+END;
