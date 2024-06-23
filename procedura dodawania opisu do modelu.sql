251bab39af88d3eff441ebb1e99e3e2530122813
Karol <karol.gladosz@gmail.com>
2024-06-23 19:59:27 +0200

    procedury dodawania opisów do modeli

---
+++ procedura dodawania opisu do modelu.sql
@@ -0,0 +1,14 @@
+﻿-- Procedura umożliwia dodanie opisu dla danego modelu produktu
+CREATE PROCEDURE [dbo].[DodajOpis]
+    @NazwaModelu NVARCHAR(50),
+    @OpisModelu NVARCHAR(500)
+AS
+BEGIN
+    -- Aktualizuj OpisModelu dla wszystkich modeli o podanej NazwaModelu
+    UPDATE dbo.Model
+    SET OpisModelu = @OpisModelu
+    WHERE NazwaModelu = @NazwaModelu;
+
+    -- Zakończ procedurę
+    RETURN;
+END;
---
+++ procedura wyboru modelu 3 dla okna dodaj opis.sql
@@ -0,0 +1,12 @@
+﻿-- Procedura umożliwia wyświetlanie modeli produktów z eliminacją duplikacji
+CREATE PROCEDURE [dbo].[WybierzModelProduktu3]
+AS
+BEGIN
+    SELECT 
+        MIN(modelid) AS modelid,
+        nazwamodelu 
+    FROM 
+        model
+    GROUP BY 
+        nazwamodelu
+END
