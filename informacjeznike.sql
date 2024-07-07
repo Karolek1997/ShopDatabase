82df3bf8925ae929530f723807eb31b8482df297
Karol Gładosz <karol.gladosz@gmail.com>
2024-07-07 13:22:39 +0200

    usuwam stara tabele

--- informacjeznike.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[Informacjeoproduktach](
-	[ID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	[Nazwa] [nvarchar](255) NULL,
-	[Podtytu³] [nvarchar](255) NULL,
-	[Aktualna_cena] [float] NULL,
-	[Pelna_cena] [float] NULL,
-	[UnikalneID] [varchar](255) NULL,
-	[Dostepnosc] [bit] NULL);
